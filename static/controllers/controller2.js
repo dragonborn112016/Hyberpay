25c5b83d0782642e56afc91b23d4f6adbe7b3ea6
chitra <chitra.joshi2012@gmail.com>
2015-11-04 17:13:38 +0530

    login add

--- static/controllers/controller2.js
+++ static/controllers/controller2.js
@@ -23,23 +23,6 @@
         /*gmail login*/
 
 
-            vm.onSignIn = function(googleUser) {
-            console.log("gmail");
-            console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
-
-            // Useful data for your client-side scripts:
-            var profile = googleUser.getBasicProfile();
-            console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-            console.log("Name: " + profile.getName());
-            console.log("Image URL: " + profile.getImageUrl());
-            console.log("Email: " + profile.getEmail());
-
-            // The ID token you need to pass to your backend:
-            var id_token = googleUser.getAuthResponse().id_token;
-            console.log("ID Token: " + id_token);
-
-        };
-
 
         vm.go = function ( path ) {
             $location.path( path );
@@ -52,15 +35,13 @@
 
         $http.get('http://localhost:8000/accounts/profile/').success(function(data) {
             vm.pre_data =angular.fromJson(data);
-            
-            console.log(vm.pre_data.label);
             for(var i=0; i<vm.pre_data.length; i++){
-            vm.data.push({sender:vm.pre_data[i].sender,date:vm.pre_data[i].date,html_content:$sce.trustAsHtml(vm.pre_data[i].html_content),ammount:vm.pre_data[i].ammount,label:vm.pre_data[i].label})
+            vm.data.push({sender:vm.pre_data[i].sender,date:vm.pre_data[i].date,html_content:$sce.trustAsHtml(vm.pre_data[i].html_content),ammount:vm.pre_data[i].ammount, label:vm.pre_data[i].label})
         }
 
         });
 
-
+        vm.data.category = "Recharge";
 
 
 
