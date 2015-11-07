6d194cfb4e402bbee67dc41f106983578ace7593
chitra <chitra.joshi2012@gmail.com>
2015-11-07 17:40:33 +0530

    order id added

--- static/controllers/controller2.js
+++ static/controllers/controller2.js
@@ -7,22 +7,20 @@
     var controllerId = 'AppCtrl';
 
     angular.module('app')
-        .controller(controllerId, ['$timeout', '$mdSidenav', '$log','$http','$location','$sce', AppCtrl]);
+        .controller(controllerId, ['$location', AppCtrl]);
 
-    function AppCtrl($timeout, $mdSidenav, $log, $http, $location, $sce) {
+    function AppCtrl( $location) {
         var vm = this;
 
         vm.activate = activate;
 
 
-        vm.video = {
-            id: 'ZSt9tm3RoUU'
-        };
+        //vm.video = {
+        //    id: 'ZSt9tm3RoUU'
+        //};
 
 
         /*gmail login*/
-
-
 
         vm.go = function ( path ) {
             $location.path( path );
@@ -30,68 +28,19 @@
 
 
 
-        vm.data=[];
-        vm.pre_data =[];
-
-        $http.get('http://localhost:8000/accounts/profile/').success(function(data) {
-            vm.pre_data =angular.fromJson(data);
-            for(var i=0; i<vm.pre_data.length; i++){
-            vm.data.push({sender:vm.pre_data[i].sender,date:vm.pre_data[i].date,html_content:$sce.trustAsHtml(vm.pre_data[i].html_content),ammount:vm.pre_data[i].ammount, label:vm.pre_data[i].label})
-        }
-
-        });
-
-        vm.data.category = "Recharge";
 
 
 
 
 
-       /* dashboard */
 
-        vm.toggleRight = buildToggler('right');
 
-        function debounce(func, wait, context) {
-            var timer;
-            return function debounced() {
-                var context = vm,
-                    args = Array.prototype.slice.call(arguments);
-                $timeout.cancel(timer);
-                timer = $timeout(function() {
-                    timer = undefined;
-                    func.apply(context, args);
-                }, wait || 10);
-            };
-        }
-        function buildToggler(navID) {
-            return debounce(function() {
-                $mdSidenav(navID).
-                    toggle().
-                    then(function () {
-                        $log.debug("toggle " + navID + " is done");
-                    });
-            }, 200);
-        }
             /* cal */
 
 
 
 
 
-        /*view */
-
-        vm.show=[];
-
-        for(var i=0;i<vm.data.length;i++){
-            vm.show[i] = true;
-        }
-        var id1;
-
-        vm.clicked = function(id)
-        {
-            vm.show[id]=!vm.show[id];
-            id1 = id;
-        };
 
 
         activate();
